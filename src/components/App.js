bc9b7ef272b3a8f706016142eabd2f36cdd6b67c
Julia <Julia@Julias-MacBook-Pro.local>
2019-03-05 13:29:31 -0500

    Integrated redux-form

--- src/components/App.js
+++ src/components/App.js
@@ -16,9 +16,9 @@ const App = () => {
             <Header />
             <Route path="/" exact component={StreamList} />
             <Route path="/streams/new" exact component={StreamCreate} />
-            <Route path="/streams/edit" component={StreamEdit} />
-            <Route path="/streams/delete" component={StreamDelete} />
-            <Route path="/streams" component={StreamShow} />
+            <Route path="/streams/edit" exact component={StreamEdit} />
+            <Route path="/streams/delete" exact component={StreamDelete} />
+            <Route path="/streams" exact component={StreamShow} />
           </div>
         </BrowserRouter>
       </div>
